19ebb2bca5cdcb093b37df601d941d2eab6d6ea7
hanr824 <297123916@qq.com>
2019-02-17 00:04:11 +0800

    Revert "Revert "测试""
    This reverts commit 1c7b6bba8f9322e39aea65786f3e3e995bc7ddf5.

--- get_text.py
+++ get_text.py
@@ -1,10 +1,15 @@
-import requests
-from lxml import etree
+
 import re 
 import time
 
+import requests
+from lxml import etree
+
 
 from config import USERID,USERNAME
+
+
+use_text=[]
 
 headers = {
 	    # "Host":"login.sina.com.cn",
@@ -15,22 +20,21 @@ headers = {
 	    "Accept-Language":"zh-CN,zh;q=0.9",
 	    }
 
-def get_page_url(url):
+def get_page_url(i):
+	'''获取每页的帖子链接'''
+	url = 'https://www.duanwenxue.com/shige/aiqingshiju/'+i
 	res = requests.get(url,headers=headers)
 	html = etree.HTML(res.text)
 	links = html.xpath('//div[@class="list-base-article"]/ul/li/a/@href')
 	for link in links:
-		url = 'https://www.duanwenxue.com'+link
-		#print(url)
-		get_text(url)
+		get_text(link)
 		
 
-def get_main_url():
-	CON = []
+def get_main():
+
+	'''获取分页链接'''
 
 	url = 'https://www.duanwenxue.com/shige/aiqingshiju/'
-	
-
 	resp = requests.get(url,headers=headers)
 	#html = etree.HTML(resp.text)
 	#con = html.xpath('//div[@class="list-base-article"]/ul/li/a/@href')
@@ -38,34 +42,30 @@ def get_main_url():
 	pattern = r'option value="(.*?)"'
 	list = re.findall(pattern,resp.text)
 	for i in list:
-		url = 'https://www.duanwenxue.com/shige/aiqingshiju/'+i
-		get_page_url(url)
+		get_page_url(i)
 		
-	
-	#print(CON)
-
-def get_text(url):
-
+def get_text(link):
+		
+	url = 'https://www.duanwenxue.com'+link
+			#print(url)
 	res= requests.get(url,headers=headers)
-
 	html = etree.HTML(res.text)
 	try:
 		texts = html.xpath('//div[@class="article-content"]/p')[0]
-	
 		t=texts.xpath('string(.)')
 		#for i in texts:
-
-		post_text(t)
+		# if t not in use_text:
+		# 	use_text.append(t)
+		# 	#print(t)
+		return t
+		# else:
+		# 	pass
+		
 	except:
 		pass
+
+			
 
 def save_text(t):
 	with open('text.txt','a',encoding='utf-8') as f:
 		f.write(t+'\n')
-
-	
-
-
-
-
-get_main_url()
