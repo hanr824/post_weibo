1898597b6fee52a697a483e21bb18d8fe4744bf6
hanr824 <297123916@qq.com>
2019-02-16 14:32:56 +0800

    修改获取文字信息的py文件

--- get_text.py
+++ get_text.py
@@ -67,53 +67,5 @@ def save_text(t):
 
 
 
-def post_text(m):
 
-	url = 'https://weibo.com/aj/mblog/add?ajwvr=6&__rnd={}'.format(int(time.time()*1000))
-	headers = {
-	"Host":"weibo.com",
-	"Connection":"keep-alive",
-	"Content-Length":"179",
-	"Origin":"https://weibo.com",
-	"X-Requested-With":"XMLHttpRequest",
-	"User-Agent":"Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
-	"Content-Type":"application/x-www-form-urlencoded",
-	"Referer":"https://weibo.com/u/{}/home?topnav=1&wvr=6".format(USERID),
-	"Accept":"*/*",
-	"Accept-Encoding":"gzip, deflate, br",
-	}
-	text = m
-	print(text)
-	data ={
-		'location':'v6_content_home',
-		'text':text,
-		'appkey':"",
-		'style_type':'1',
-		'pic_id':'',
-		'tid':'',
-		'pdetail':'',
-		'mid':'',
-		'isReEdit':'false',		
-		'rank':'0',
-		'rankid':'',
-		'module':"stissue",
-		'pub_source':'main_',		
-		'pub_type':'dialog',
-		'isPri':'0',
-		'_t':'0',
-			}
-	with open("cookies",'r') as f:
-		content = f.read()
-	cookies =eval(content)
-	print(cookies)
-	resp = requests.post(url=url,cookies=cookies,headers=headers,data=data)
-	#print(resp.text)
-	if resp.status_code ==200:
-		print("succesful")
-		
-		
-	else:
-		print('not successful')
-	
-	time.sleep(100)	
 get_main_url()
